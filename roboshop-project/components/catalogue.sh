0964d134fa2ac76e64146d45b27edf83ec6299b5
prasad <prasadpamidi579@gmail.com>
2022-08-22 07:21:18 +0530

    catalogue4

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -3,7 +3,7 @@
 source /components/common.sh
 
 echo "setup NodeJS"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
 
 echo "Install Nodejs"
 yum install nodejs -y &>>$LOG_FILE
