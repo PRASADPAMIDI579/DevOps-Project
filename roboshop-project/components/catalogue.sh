70475799c56704b3b0b97e3a25d4f91dc5ca3c15
prasad <prasadpamidi579@gmail.com>
2022-08-24 06:51:16 +0530

    catalogue2

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -3,7 +3,7 @@
 source /components/common.sh
 
 echo "setup NodeJS"
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
 
 echo "Install Nodejs"
 yum install nodejs -y &>>$LOG_FILE
@@ -21,10 +21,11 @@ unzip -o catalogue.zip &>>$LOG_FILE
 echo "clean old catalogue"
 rm -rf /home/roboshop/catalogue $>>$LOG_FILE
 
-echo "copy cataloge content"
+echo "copy catalogue content"
 cp -r catalogue-main /home/roboshop/catalogue &>>$LOG_FILE
 
 echo "install nodejs depedendcies"
+cd /home/roboshop/catalogue
 npm install &>>$LOG_FILE
 
 chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
