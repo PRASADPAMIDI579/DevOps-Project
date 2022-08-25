c1f63a5519a8817571f36ba94837e7a52f611895
prasad <prasadpamidi579@gmail.com>
2022-08-25 11:17:48 +0530

    catalogue4

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -7,3 +7,9 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
 
 echo "Install Nodejs"
 yum install nodejs -y &>>$LOG_FILE
+
+echo "create app user"
+useradd roboshop &>>$LOG_FILE
+
+echo "download catalogue code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
