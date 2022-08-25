cada4fa122cca280052fa69bd7bd355147349e67
prasad <prasadpamidi579@gmail.com>
2022-08-25 12:56:59 +0530

    catalouge

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -30,5 +30,15 @@ npm install &>>$LOG_FILE
 
 chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
 
+echo "Update SystemD file"
+sed -i -e 's/MONGO DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/SystemD.service &>>$LOG_FILE
+
+echo "Setup Catalogue SystemD file"
+mv /home/roboshop/catalogue/SystemD.service /etc/systemd/system/catalogue.service &>>LOG_FILE
+
+echo "start catalogue"
+systemctl daemon-reload &>>LOG_FILE
+systemctl enable catalogue &>>LOG_FILE
+systemctl start catalogue &>>LOG_FILE
 
 
