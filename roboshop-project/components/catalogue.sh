b73fcee88f31ebc7c31392a56a87dd3a1ab82f5a
prasad <prasadpamidi579@gmail.com>
2022-08-25 11:24:00 +0530

    catalogue6

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -17,3 +17,12 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
 echo "extract catalogue code"
 cd /tmp/
 unzip -o catalogue.zip &>>$LOG_FILE
+
+echo "copy catalogue content"
+cp -r catalogue-main /home/roboshop/catalogue &>>$LOG_FILE
+
+echo "install nodejs depedendcies"
+cd /home/roboshop/catalogue
+npm install &>>$LOG_FILE
+
+
