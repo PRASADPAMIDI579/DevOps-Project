e61eb9b37af6a8da23b97a55994900c1829728df
prasad <prasadpamidi579@gmail.com>
2022-09-02 23:15:32 +0530

    Instance1

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -7,4 +7,4 @@ if [ -z "${AMI_ID}" ]; then
     echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output.text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text
