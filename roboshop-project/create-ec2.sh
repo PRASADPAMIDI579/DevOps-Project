026c4a27a0d690977fd0f805b09d8dc44c440a9c
prasad <prasadpamidi579@gmail.com>
2022-09-03 00:12:35 +0530

    Instance creating2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -7,14 +7,15 @@ if [ -z "${INSTANCE_NAME}" ]; then
 fi    
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+
 if [ -z "${AMI_ID}" ]; then
-    echo -e "\e[1;31mUnable to find image AMIID\e[0m"
+    echo -e "\e[1;31mUnable to find image AMI_ID\e[0m"
     else
     echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
     exit
 fi
 
-PRIVATE_IP=$(aws ec2 describe-instances --filters Name=instance-type,Values=${INSTANCE_NAME} --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
 if [ -z "${PRIVATE_IP}" ]; then
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${INSTANCE_NAME}}]"
