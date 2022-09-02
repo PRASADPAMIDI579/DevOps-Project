60b88f579b3b0facd417614ef68a0c4e9f3bc64b
prasad <prasadpamidi579@gmail.com>
2022-09-02 12:18:54 +0530

    create ec2 ami

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters" Name=name,Values=Centos-8-DevOps-Practice --query 'Images[*].[ImageId]' --output text)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
-echo "unable to find AMIID"
+    echo "Unable to find image AMIID"
 fi
