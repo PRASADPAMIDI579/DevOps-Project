0c03d32cc0f897a531dc0a743e22993d63922819
prasad <prasadpamidi579@gmail.com>
2022-09-17 12:17:46 +0530

    Roboshop.sh1

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+ ID=$(id -u)
+ if [ $ID -ne 0 ]; then
+    echo -e "\e[1;31m You should be root usetr to excute this script..\e[0m"
+    exit
+fi
+
 if [ -f components/$1.sh ]; then
 bash components/$1.sh
 else
-    echo -e "\e31mInvalid Input\e[0m"
-    echo -e "\e33mAvailable Inputs - frontend|mongodb|catalogue|redis|users|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+    echo -e "\e[1;31mInvalid Input\e[0m"
+    echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|catalogue|redis|users|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
 fi
