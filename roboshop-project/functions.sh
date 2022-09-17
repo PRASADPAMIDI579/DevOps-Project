05a2413f06ce52eaf48f83895deeb27338e4e8ea
prasad <prasadpamidi579@gmail.com>
2022-09-17 23:46:11 +0530

    functions

---
+++ roboshop-project/functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# a set of data to a name is called as variable
+# a set of commands to a name is called as function
+
+## Declare a function
+SAMPLE()
+#a=20
+#b=20
+echo "WELCOME TO DevOps Ttaining"
+echo "WELCOME TO DevOps Ttaining"
+#return 1
+echo "WELCOME TO DevOps Ttaining"
+echo "WELCOME TO DevOps Ttaining"
+
+## main progarm
+## call the function
+#a=10
+SAMPLE
+SAMPLE
