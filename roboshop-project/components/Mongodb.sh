adb819a5257c0f61e7f1df4343b1a9a5fec0e05c
prasad <prasadpamidi579@gmail.com>
2022-09-18 00:42:49 +0530

    Mongodb validating status

--- roboshop-project/components/Mongodb.sh
+++ roboshop-project/components/Mongodb.sh
@@ -4,28 +4,35 @@ source components/common.sh
 
 echo "Download Mongodb repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+STAT $?
 
 echo "install MongoDB"
 yum install -y mongodb-org
+STAT $?
 
 echo "update Mongodb Config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+STAT $?
 
 echo "start database"
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
+STAT $?
 
 echo "Download schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+STAT $?
 
 echo "Extract schema"
 cd /tmp
 unzip -o mongodb.zip &>>$LOG_FILE
+STAT $?
 
 echo "Load schema"
 cd mongodb-main
 mongo < catalogue.js &>>$LOG_FILE
 mongo < users.js &>>$LOG_FILE
+STAT $?
 
 
 
--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -4,42 +4,52 @@ source components/common.sh
 
 echo "setup NodeJS"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+STAT $?
 
 echo "Install Nodejs"
 yum install nodejs -y &>>$LOG_FILE
+STAT $?
 
 echo "create app user"
 useradd roboshop &>>$LOG_FILE
+STAT $?
 
 echo "download catalogue code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
+STAT $?
 
 echo "extract catalogue code"
 cd /tmp/
 unzip -o catalogue.zip &>>$LOG_FILE
+STAT $?
 
 echo "clean old catalogue"
 rm -rf /home/roboshop/catalogue $>>$LOG_FILE
+STAT $?
 
 echo "copy catalogue content"
 cp -r catalogue-main /home/roboshop/catalogue &>>$LOG_FILE
+STAT $?
 
 echo "install nodejs depedendcies"
 cd /home/roboshop/catalogue
 npm install &>>$LOG_FILE
+STAT $?
 
 chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
 
 echo "Update SystemD file"
 sed -i -e 's/MONGO DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/SystemD.service &>>$LOG_FILE
+STAT $?
 
 echo "Setup Catalogue SystemD file"
 mv /home/roboshop/catalogue/SystemD.service /etc/systemd/system/catalogue.service &>>LOG_FILE
+STAT $?
 
 echo "start catalogue"
 systemctl daemon-reload &>>LOG_FILE
 systemctl enable catalogue &>>LOG_FILE
 systemctl start catalogue &>>LOG_FILE
-
+STAT $?
 
  
