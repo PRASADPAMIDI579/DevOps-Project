2db4f852f6394c0d8f03312a1133e8935a89892b
prasad <prasadpamidi579@gmail.com>
2022-09-18 01:17:56 +0530

    ec2 centos 7

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -8,7 +8,7 @@ if [ -z "${INSTANCE_NAME}" ]; then
     exit
 fi    
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z "${AMI_ID}" ]; then
     echo -e "\e[1;31mUnable to find image AMI_ID\e[0m"
