85663979de4a4ba44241bc15b629c7bc5f63cfc4
prasad <prasadpamidi579@gmail.com>
2022-09-17 12:12:04 +0530

    Roboshop.sh

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ -f components/$1.sh ]; then
 bash components/$1.sh
+else
+    echo -e "\e31mInvalid Input\e[0m"
+    echo -e "\e33mAvailable Inputs - frontend|mongodb|catalogue|redis|users|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+fi
