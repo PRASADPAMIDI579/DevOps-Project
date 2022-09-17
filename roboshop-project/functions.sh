dc1d5fb664118174f06c5d761f919021507cde17
prasad <prasadpamidi579@gmail.com>
2022-09-17 23:52:43 +0530

    functions1

--- roboshop-project/functions.sh
+++ roboshop-project/functions.sh
@@ -4,14 +4,18 @@
 # a set of commands to a name is called as function
 
 ## Declare a function
-SAMPLE()
+SAMPLE() {
+
 #a=20
 #b=20
 echo "WELCOME TO DevOps Ttaining"
-echo "WELCOME TO DevOps Ttaining"
+#echo "WELCOME TO DevOps Ttaining"
 #return 1
-echo "WELCOME TO DevOps Ttaining"
-echo "WELCOME TO DevOps Ttaining"
+#echo "WELCOME TO DevOps Ttaining"
+#echo "WELCOME TO DevOps Ttaining"
+
+}
+
 
 ## main progarm
 ## call the function
