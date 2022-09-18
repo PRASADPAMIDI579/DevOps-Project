244c8ce13e1acc3bb0a63a654a783fde264da0ab
prasad <prasadpamidi579@gmail.com>
2022-09-19 02:43:12 +0530

    user

---
+++ roboshop-project/components/users.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+source components/common.sh
+
+echo "setup NodeJS"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+STAT $?
+
+echo "Install Nodejs"
+yum install nodejs -y &>>$LOG_FILE
+STAT $?
+
+
+
+echo "create app user"
+useradd roboshop &>>$LOG_FILE
+STAT $?
+
+echo "download users code"
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG_FILE
+STAT $?
+
+echo "extract user code"
+cd /tmp/
+unzip -o user.zip &>>$LOG_FILE
+STAT $?
+
+echo "clean old user"
+rm -rf /home/roboshop/user $>>$LOG_FILE
+STAT $?
+
+
+
+echo "copy user content"
+cp -r user-main /home/roboshop/user &>>$LOG_FILE
+STAT $?
+
+echo "install nodejs depedendcies"
+cd /home/roboshop/user
+npm install &>>$LOG_FILE
+STAT $?
+
+chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
+
+echo "Update SystemD file"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/user/SystemD.service &>>$LOG_FILE
+STAT $?
+
+echo "Setup user SystemD file"
+mv /home/roboshop/user/SystemD.service /etc/systemd/system/user.service &>>LOG_FILE
+STAT $?
+
+echo "start user"
+systemctl daemon-reload &>>LOG_FILE
+systemctl enable user &>>LOG_FILE
+systemctl start user &>>LOG_FILE
+STAT $?
+
+ 
