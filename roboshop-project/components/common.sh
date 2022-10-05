d381602a1a9653de2ccce510cf68a6de542f0831
prasad <prasadpamidi579@gmail.com>
2022-10-05 11:39:28 +0530

    dispatch

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -132,23 +132,8 @@ GOLANG() {
     go mod init dispatch &>>$LOG_FILE
     go get &>>$LOG_FILE
     go build &>>$LOG_FILE
+    STAT
 }
-
-
-
-1. Switch to roboshop user and perform the following commands.
-
-```bash
-$ curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
-$ unzip /tmp/dispatch.zip 
-$ mv dispatch-main dispatch 
-$ cd dispatch 
-$ go mod init dispatch
-$ go get 
-$ go build
-```
-
-1. Update the systemd file and configure the dispatch service in systemd
 
 
 
